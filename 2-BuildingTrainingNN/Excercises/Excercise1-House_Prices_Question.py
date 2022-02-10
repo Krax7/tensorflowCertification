95f014fa8bde3a03214690cff18e44c84115dc3d
Omar Hoyos <omar@devf.mx>
2022-02-10 17:57:50 -0600

    Split into questions and solutions

--- 2-BuildingTrainingNN/Excercises/Excercise1-House_Prices_Question.py
+++ 2-BuildingTrainingNN/Excercises/Excercise1-House_Prices_Question.py
@@ -9,55 +9,21 @@
 # 
 # Hint: Your network might work better if you scale the house price down. You don't have to give the answer 400...it might be better to create something that predicts the number 4, and then your answer is in the 'hundreds of thousands' etc.
 
-# In[1]:
-
-
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
-
-
-# In[2]:
-
 
 # GRADED FUNCTION: house_model
 def house_model(y_new):
     xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
     ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0, 3.5])
-    model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
-    model.compile(optimizer='sgd', loss='mean_squared_error')
-    model.fit(xs, ys, epochs=500)
+    # YOUR CODE SHOULD START HERE
+
+    # YOUR CODE SHOULD END HERE
+    
     return model.predict(y_new)[0]
-
-
-# In[3]:
-
 
 prediction = house_model([7.0])
 print(prediction)
 
-
-# In[4]:
-
-
-# Now click the 'Submit Assignment' button above.
-# Once that is complete, please run the following two cells to save your work and close the notebook
-
-
-# In[4]:
-
-
-get_ipython().run_cell_magic('javascript', '', '<!-- Save the notebook -->\nIPython.notebook.save_checkpoint();')
-
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('javascript', '', 'IPython.notebook.session.delete();\nwindow.onbeforeunload = null\nsetTimeout(function() { window.close(); }, 1000);')
-
-
-# In[ ]:
-
-
-
-
+# Expected Output: A number closed to 4.0
--- 2-BuildingTrainingNN/Solutions/Excercise2-Solution.py
+++ 2-BuildingTrainingNN/Solutions/Excercise2-Solution.py
@@ -16,7 +16,7 @@ from os import path, getcwd, chdir
 # DO NOT CHANGE THE LINE BELOW. If you are developing in a local
 # environment, then grab mnist.npz from the Coursera Jupyter Notebook
 # and place it inside a local folder and edit the path to that location
-# path = f"{getcwd()}/../tmp2/mnist.npz"
+path = f"{getcwd()}/../tmp2/mnist.npz"
 
 # GRADED FUNCTION: train_mnist
 def train_mnist():
@@ -24,6 +24,11 @@ def train_mnist():
     # please do not remove # model fitting inline comments.
 
     # YOUR CODE SHOULD START HERE
+    class myCallback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs={}):
+            if(logs.get('acc')>0.99):
+                print('Reached 99% accuracy so cancelling training!')
+                self.model.stop_training = True
 
     # YOUR CODE SHOULD END HERE
 
@@ -31,11 +36,17 @@ def train_mnist():
 
     (x_train, y_train),(x_test, y_test) = mnist.load_data(path=path)
     # YOUR CODE SHOULD START HERE
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+    
+    callbacks = myCallback()
     
     # YOUR CODE SHOULD END HERE
     model = tf.keras.models.Sequential([
         # YOUR CODE SHOULD START HERE
-        
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, tf.nn.relu),
+        tf.keras.layers.Dense(10, tf.nn.softmax)
         # YOUR CODE SHOULD END HERE
     ])
 
@@ -45,7 +56,7 @@ def train_mnist():
     
     # model fitting
     history = model.fit(# YOUR CODE SHOULD START HERE
-                        
+        x_train, y_train, callbacks=[callbacks], epochs=10
               # YOUR CODE SHOULD END HERE
     )
     # model fitting
